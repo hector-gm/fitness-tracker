ca72631454a664ea4fde6b25228cbc37aa19e7dd
Hector Garcia Millares <hgarcia.millares@gmail.com>
2021-02-09 23:18:11 -0500

    Added console log for server connection

--- server.js
+++ server.js
@@ -10,3 +10,5 @@ app.use(express.static('./public'));
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+
+app.listen(PORT, () => console.log(`App listening at: http://localhost:${PORT}`));
