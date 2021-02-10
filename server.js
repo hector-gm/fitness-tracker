bd4209f22f56369cd7b88240ab2b7b3083ac9eb8
Hector Garcia Millares <hgarcia.millares@gmail.com>
2021-02-09 23:16:32 -0500

    Set up server connection

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('./public'));
+
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
