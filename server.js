1f7545ba1f58b254512d5d14344792c909013f87
Hector Garcia Millares <hgarcia.millares@gmail.com>
2021-02-09 23:48:01 -0500

    Added html routes to server.js and tested to ensure they function correctly when launching app

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
 
+const htmlRoutes = require('./routes/html-routes');
+
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('./public'));
+app.use(htmlRoutes);
 
 const mongoose = require('mongoose');
 
