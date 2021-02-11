afb632a6df845c71f8ea25b753ae0c1bb2888ba8
Hector Garcia Millares <hgarcia.millares@gmail.com>
2021-02-11 17:22:17 -0500

    Adding access requirement to api routes in server.js

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ const express = require('express');
 const app = express();
 
 const htmlRoutes = require('./routes/html-routes');
+const apiRoutes = require('./routes/api-routes');
 
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('./public'));
-app.use(htmlRoutes);
+app.use(htmlRoutes, apiRoutes);
 
 const mongoose = require('mongoose');
 
